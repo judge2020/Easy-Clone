e6a825319a7be734fe4413c2f426497637afdae8
judge2020 <hunterray55@hotmail.com>
2016-12-25 21:13:18 -0500

    Second Commit

--- Easy-Clone/Program.cs
+++ Easy-Clone/Program.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Easy_Clone
 {
-	class Program
+	static class Program
 	{
-		static void Main(string[] args)
+		static void Main()
 		{
+
+
+			System.Diagnostics.Process process = new System.Diagnostics.Process();
+			System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo
+			{
+				WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
+				FileName = "cmd.exe",
+				Arguments = "/C git clone" + Clipboard.GetText()
+			};
+			process.StartInfo = startInfo;
+			process.Start();
 		}
 	}
 }
