8573404d6cbb1a8967c63a281780c23897e5d9e2
judge2020 <hunterray55@hotmail.com>
2017-01-05 12:01:53 -0500

    v1

--- Easy-Clone/Program.cs
+++ Easy-Clone/Program.cs
@@ -1,34 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Easy_Clone
 {
 	static class Program
 	{
-		static void Main()
+		[STAThread]
+		private static void Main()
 		{
-
-
 			try
 			{
+				var clipboard = (string) Clipboard.GetData(DataFormats.UnicodeText);
+				var reponame = clipboard.Split('/');
+				var path = Application.StartupPath + "\\" + reponame.Last().Replace(".git", "");
 				Process.Start("cmd.exe",
-					" /k 'git clone " + Clipboard.GetText(TextDataFormat.Text) + " " +
-					System.Reflection.Assembly.GetEntryAssembly().Location);
-
+					" /k git clone " + clipboard + " " +
+					path + " && exit");
 			}
-			catch(Exception e)
+			catch (Exception e)
 			{
-
-				MessageBox.Show("Please make sure you have text in clipboard." + e.Message);
+				MessageBox.Show("Please make sure you have text in clipboard. " + e.Message);
 			}
-			while ( Console.ReadLine() == "")
+			finally
 			{
-				
+				Environment.Exit(0);
 			}
 		}
 	}
